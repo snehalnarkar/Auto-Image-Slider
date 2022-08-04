d66559c592de764ab37349e0f8a948bc655d4b58
https://github.com/snehalnarkar/DrawerMenu.git <https://github.com/snehalnarkar/DrawerMenu.git>
2022-08-04 12:18:27 +0530

    image slider

---
+++ app/src/main/java/com/example/autoimageslider/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.autoimageslider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton.OnChangedCallback
+import java.lang.Math.abs
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var  handler: Handler
+    private lateinit var imageList: ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        init()
+        setUpTransformer()
+
+        viewPager2.registerOnPageChangeCallback(object : OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable,2000)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable,2000)
+    }
+    private  val runnable =Runnable{
+        viewPager2.currentItem = viewPager2.currentItem + 1
+
+    }
+    private fun setUpTransformer(){
+        val transformer =CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY=0.85f + r * 0.14f
+
+        }
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init(){
+        viewPager2 =findViewById(R.id.viewPager2)
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.cloth)
+        imageList.add(R.drawable.offer1)
+        imageList.add(R.drawable.offer2)
+
+        adapter= ImageAdapter(imageList,viewPager2)
+
+        viewPager2.adapter=adapter
+        viewPager2.offscreenPageLimit =3
+        viewPager2.clipToPadding =false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode =RecyclerView.OVER_SCROLL_NEVER
+
+    }
+}
